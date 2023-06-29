81c35da4338407e76a6b95120de735478c6ff939
alexneto2022 <alex.neto@hcpgestao.org.br>
2023-06-29 11:39:03 -0300

    Curso React: Estruturando o projeto - #18

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,10 +1,52 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </ul>
+      <Routes>
+        <Route
+          exact path="/"
+          element={
+            <Container customClass="min-height">
+              <Home />
+            </Container>
+          } />
+        <Route
+          exact path="/company"
+          element={
+            <Container customClass="min-height">
+              <Company />
+            </Container>
+          } />
+        <Route
+          exact path="/contact"
+          element={
+            <Container customClass="min-height">
+              <Contact />
+            </Container>
+          } />
+        <Route
+          exact path="/newproject"
+          element={
+            <Container customClass="min-height">
+              <NewProject />
+            </Container>
+          } />
+
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
